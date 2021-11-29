c552f91580188f67560d661bac9f5e6186a6346e
Christian <c_wance@msn.com>
2021-11-28 19:28:20 -0800

    Chapter 6

--- pyt2_allfiles/python/exercises/ch06/movie_list_2d.py
+++ pyt2_allfiles/python/exercises/ch06/movie_list_2d.py
@@ -6,40 +6,54 @@ def list(movie_list):
         return
     else:
         for i, movie in enumerate(movie_list, start=1):
-            print(f"{i}. {movie[0]} ({movie[1]})")
+            print(f"{i}. {movie[0]} ({movie[1]}) @ ${movie[2]}")
         print()
+
 
 def add(movie_list):
     name = input("Name: ")
     year = input("Year: ")
-    movie = [name, year]
+    price = input("Price: ")
+    movie = [name, year, price]
     movie_list.append(movie)
     print(f"{movie[0]} was added.\n")
-    
+
+
 def delete(movie_list):
     number = int(input("Number: "))
     if number < 1 or number > len(movie_list):
         print("Invalid movie number.\n")
     else:
-        movie = movie_list.pop(number-1)
+        movie = movie_list.pop(number - 1)
         print(f"{movie[0]} was deleted.\n")
+
+
+def find(movie_list):
+    year = input("Year: ")
+    for movie in movie_list:
+        if year == str(movie[1]):
+            print(f"{movie[0]} was released in {year}")
+
+
 
 def display_menu():
     print("COMMAND MENU")
     print("list - List all movies")
     print("add -  Add a movie")
     print("del -  Delete a movie")
+    print("find -  Find movies by year")
     print("exit - Exit program")
     print()
-    
+
+
 def main():
-    movie_list = [["Monty Python and the Holy Grail", 1975],
-                  ["On the Waterfront", 1954],
-                  ["Cat on a Hot Tin Roof", 1958]]
-    
+    movie_list = [["Monty Python and the Holy Grail", 1975, 9.95],
+                  ["On the Waterfront", 1954, 5.59],
+                  ["Cat on a Hot Tin Roof", 1958, 7.95]]
+
     display_menu()
-    
-    while True:        
+
+    while True:
         command = input("Command: ")
         if command.lower() == "list":
             list(movie_list)
@@ -47,11 +61,14 @@ def main():
             add(movie_list)
         elif command.lower() == "del":
             delete(movie_list)
+        elif command.lower() == "find":
+            find(movie_list)
         elif command.lower() == "exit":
             break
         else:
             print("Not a valid command. Please try again.\n")
     print("Bye!")
+
 
 if __name__ == "__main__":
     main()
--- pyt2_allfiles/python/exercises/ch06/test_scores.py
+++ pyt2_allfiles/python/exercises/ch06/test_scores.py
@@ -5,41 +5,45 @@ def display_welcome():
     print("Enter 'x' to exit")
     print("")
 
+
 def get_scores():
-    score_total = 0
-    counter = 0
+    scores = []
     while True:
         score = input("Enter test score: ")
         if score == "x":
-            return  score_total, counter
+            return scores
         else:
             score = int(score)
-            if score >= 0 and score <= 100:
-                score_total += score
-                counter += 1 
+            if 0 <= score <= 100:
+                scores.append(score)
             else:
                 print("Test score must be from 0 through 100. " +
                       "Score discarded. Try again.")
 
-def process_scores(score_total, count):
+
+def process_scores(scores):
+    scores.sort()
     # calculate average score
-    average = score_total / count
-                
+    average = sum(scores) / len(scores)
+
     # format and display the result
     print()
-    print("Score total:       ", score_total)
-    print("Number of Scores:  ", count)
+    print("Score total:       ", sum(scores))
+    print("Number of Scores:  ", len(scores))
     print("Average Score:     ", average)
+    print("Low Score:         ", scores[0])
+    print("High Score:        ", scores[len(scores) - 1])
+    print("Median Score:      ", scores[int(len(scores)/2)])
+
 
 def main():
     display_welcome()
-    score_total, count = get_scores()
-    process_scores(score_total, count)
+    score_total= get_scores()
+    process_scores(score_total)
     print("")
     print("Bye!")
+
 
 # if started as the main module, call the main function
 if __name__ == "__main__":
     main()
-
-
